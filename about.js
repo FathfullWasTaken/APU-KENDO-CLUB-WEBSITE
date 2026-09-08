c2271de767b2ad427a2c7fefe01d0664dbd2d79b
dannon111 <dannon20086776@gmail.com>
2026-09-08 16:24:09 +0800

    Create about.js

---
+++ about.js
@@ -0,0 +1,11 @@
+let time = new Date();
+let hour = time.getHours();
+
+if (hour < 12)
+{
+    alert("Good morning, welcome to Kendo Club join us now ");
+}
+else
+{
+    alert("Good afternoon, welcome to Kendo Club join us now");
+}
